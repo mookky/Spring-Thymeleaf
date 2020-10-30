911c8c3c7130a71d4f63c5b8197dd6e2bd5bc1ee
mookky <72590407+mookky@users.noreply.github.com>
2020-10-30 15:52:45 +0700

    Add files via upload

---
+++ atm/src/main/java/th/ac/kmitl/atm/AtmApplication.java
@@ -0,0 +1,15 @@
+package th.ac.kmitl.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class AtmApplication {
+
+	public static void main(String[] args) {
+		run(AtmApplication.class, args);
+	}
+
+}
